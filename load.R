4adf24d829db52b9b90ae311b8293dbd06200f69
Manon Baha <95853515+manonb42@users.noreply.github.com>
2025-03-11 17:59:55 +0100

    [load.R] final commit

--- load.R
+++ load.R
@@ -4,56 +4,53 @@ library(caret)
 library(quadprog)
 library(xgboost)
 library(ranger)
+library(lightgbm)
 
 source("init.R")
-
+source("experts.R")
 
 tau = 0.8
 
-solar <- list()
+load <- list()
 
 # generalized additive model
 
-gam.fit <- gam(Solar_power ~
-        te(Temp, Nebulosity, k = c(12,12)) +
-        s(Solar_power.1) +
-        s(Solar_power.7) +
-        as.factor(WeekOfYear) +
-        WeekDays + DLS + Summer_break +
-        Holiday + HolidayA + HolidayB + HolidayC,
+gam.fit <- gam(Load ~
+      te(Temp, Wind, k=c(12,12))
+    + s(Load.1)
+    + s(Load.7)
+    + as.factor(WeekOfYear)
+    + WeekDays + DLS + Summer_break
+    + Holiday + HolidayA + HolidayB + HolidayC,
     data = train)
 
-solar$long <- predict(gam.fit, newdata = test)
-
-
-
+load$long <- predict(gam.fit, newdata = test)
 
 
 # medium term model
 
-medium.fit <- gam(Solar_power ~
+medium.fit <- gam(Load ~
       te(Temp, Wind, k=c(12,12))
-    + s(Solar_power.1)
-    + s(Solar_power.7)
+    + s(Load.1)
+    + s(Load.7)
     + s(WeekOfYear, bs="cc")
     + WeekDays + DLS + Summer_break
     + Holiday + HolidayA + HolidayB + HolidayC,
     data = train)
 
-solar$medium <- predict(medium.fit, newdata=data)
-data$SolarMedium <- solar$medium
-data$SolarResiduals <- data$Solar_power - data$SolarMedium
+load$medium <- predict(medium.fit, newdata=data)
+data$LoadMedium <- load$medium
+data$LoadResiduals <- data$Load - data$LoadMedium
 
-label <- data$SolarResiduals
+label <- data$LoadResiduals
 
 
 # Residuals correction
 
-
 features <- c("NumDate", "Temp", "Temp_s99_min", "Temp_s99_max",
               "Wind", "Wind_weighted", "Nebulosity", "Nebulosity_weighted",
               "Load.1", "Load.7", "Net_demand.1", "Net_demand.7",
-              "Solar_power.1", "Solar_power.7", "Wind_power.1", "Wind_power.7",
+              "Wind_power.1", "Wind_power.7", "Solar_power.1", "Solar_power.7",
               "DayOfYear")
 
 categorical_vars <- c("DayOfWeek", "DLS", "Holiday", "BH")
@@ -61,17 +58,16 @@ categorical_vars <- c("DayOfWeek", "DLS", "Holiday", "BH")
 all_vars <- c(features, categorical_vars)
 
 
-# Gradient boosting model
-
 ## LightGBM
-solar$lgb <- data$SolarMedium[test_idx] + lightbm(train, label[train_idx], test, all_vars)
+load$lgb <- data$LoadMedium[test_idx] + lightbm(
+  train, data$LoadResiduals[train_idx], test, all_vars)
 
-solar$lgb_ol <- data$SolarMedium[test_idx] + semi_online(
+load$lgb_ol <- data$LoadMedium[test_idx] + semi_online(
   function (train, label, test) { lightbm(train, label, test, all_vars) },
   data, label, nrow(train)+1)
 
 
-# GBM
+# Gradient boosting model
 
 gbm.fit <- train(Load ~
       Temp
@@ -81,33 +77,32 @@ gbm.fit <- train(Load ~
     data = train,
     method = "gbm")
 
-solar$gbm <- predict(gbm.fit, newdata = test)
+load$gbm <- predict(gbm.fit, newdata = test)
 
 
 # QRF on residuals
 
-
 qrf <- function(data, label_, test) {
-
-  model <- ranger(SolarResiduals ~
-            Wind
-          + Temp
-          + Solar_power.1 + Solar_power.7
-          + DayOfWeek,
-    data = data[train_idx, ],
-    importance = "permutation",
-    quantreg = TRUE,
-    num.trees = 500)
+  
+  model <- ranger(LoadResiduals ~
+         Wind
+       + Temp
+       + Load.1 + Load.7
+       + DayOfWeek,
+       data = data,
+       importance = "permutation",
+       quantreg = TRUE,
+       num.trees = 500)
 
   predict(model,
-          data = data[test_idx,],
+          data = test,
           quantiles = tau,
           type = "quantiles")$predictions[,]
 
-}
 
-solar$qrf_ol <- data$SolarMedium[test_idx] + semi_online(qrf, data, label, nrow(train)+1)
+} 
 
+load$qrf_ol <- data$LoadMedium[test_idx] + semi_online(qrf, data, label, nrow(train)+1)
 
 
 # XGBoost semi-online et normal
@@ -115,33 +110,36 @@ solar$qrf_ol <- data$SolarMedium[test_idx] + semi_online(qrf, data, label, nrow(
 xgb_rsme <- function(train, label, test){ xgboost_rsme(train, label, test, all_vars) }
 xgb_quant <- function(train, label, test){ xgboost_quant(train, label, test, all_vars) }
 
-solar$xgb <- data$SolarMedium[test_idx] + xgb_rsme(train, label[train_idx], test)
-solar$xgb_ol <- data$SolarMedium[test_idx] + semi_online(xgb_rsme, data, label, nrow(train)+1)
+load$xgb <- data$LoadMedium[test_idx] + xgb_rsme(train, label[train_idx], test)
+load$xgb_ol <- data$LoadMedium[test_idx] + semi_online(xgb_rsme, data, label, nrow(train)+1)
 
-solar$qxgb <- data$SolarMedium[test_idx] + xgb_rsme(train, label[train_idx], test)
+
+load$qxgb <- data$LoadMedium[test_idx] + xgb_rsme(train, label[train_idx], test)
+
 
 # Aggregation
 
 experts <- cbind(
-  xgb    = solar$xgb,
-  xgb_ol = solar$xgb_ol,
-  qxgb   = solar$qxgb,
-  lgb    = solar$lgb,
-  lgb_ol = solar$lgb_ol,
-  qrf_ol = solar$qrf_ol,
-  long   = solar$long
+  xgb    = load$xgb,
+  xgb_ol = load$xgb_ol,
+  qxgb   = load$qxgb,
+  lgb    = load$lgb,
+  lgb_ol = load$lgb_ol,
+  qrf_ol = load$qrf_ol,
+  long   = load$long
+  
 )
 
 available <- 1:nrow(test)-1
-mix <- mixture(model = "MLpol")
-mix <- predict(mix, newexpert = experts[available,], newY = test$Solar_power[available], online = TRUE)
+mix <- mixture(model = "MLpol", loss.type = list(name = "pinball", tau = 0.8))
+mix <- predict(mix, newexpert = experts[available,], newY = test$Load[available], online = TRUE)
 last_pred <- predict(mix, newexperts = experts[-available, ], online = FALSE, type = 'response')
 
-solar$final <- rbind(mix$prediction, last_pred)
-
+load$final <- rbind(mix$prediction, last_pred)
+predictions.load <- load$final
 
 # Oracle
 plot(mix)
 
-plot_predictions(solar$final, test[available,], "Solar_power")
 
+plot_predictions(load$final, test[available,], "Load")
